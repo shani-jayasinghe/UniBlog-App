eb6f1d5ec89fdafff87ce0c6d1c8bb3f4088836a
shani jayasinghe <chasha2000814@gmail.com>
2024-06-11 02:12:52 +0530

    ceate pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route }  from "react-router-dom"
+import Home from "./Pages/Home"
+import Dashboard from "./Pages/Dashboard"
+import SignIn from "./Pages/SignIn"
+import SignUp from "./Pages/SignUp"
+import About from "./Pages/About"
+import Projects from "./Pages/Projects"
+
+
 
 export default function App() {
   return (
-    <h1 className='text-3xl  text-red-500'>App</h1>
+    <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/About" element={<About />} />
+         <Route path="/SignIn" element={<SignIn />} />
+         <Route path="/SignUp" element={<SignUp />} />
+         <Route path="/Dashboard" element={<Dashboard />} />
+         <Route path="/Projects" element={<Projects />} />
+         
+          
+      </Routes>
+    </BrowserRouter>
   )
 }
 
