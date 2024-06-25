85f923714e23e004c3515c596d2f2e4961c8e82d
shani jayasinghe <shaanijayasinghe14@gmail.com>
2024-06-25 18:21:23 +0530

    create a test Api route(2)

--- Api/index.js
+++ Api/index.js
@@ -18,6 +18,8 @@ mongoose
 
 const app = express();
 
+
+
 app.listen(3000, () => {
     console.log('Sever is running on port 3000');
 }); 
--- Api/routes/user.route.js
+++ Api/routes/user.route.js
@@ -5,4 +5,4 @@ const router = express.Router();
 
 router.get('/test',test );
 
-export default router;
+export default router;  
