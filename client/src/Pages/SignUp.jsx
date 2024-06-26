49aee8d8a27933142c6c5fd779387f772d5ab905
shani jayasinghe <shaanijayasinghe14@gmail.com>
2024-06-26 23:48:45 +0530

    add funcationality to the signup page

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,9 +1,43 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert,Button, Label, TextInput,Spinner } from 'flowbite-react'
+import  { useState } from 'react'
+import { Link, useNavigate} from 'react-router-dom'
 
 
 export default function SignUp() {
+
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value.trim()});
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try{
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/Api/auth/SignUp', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/SignIN');
+      }
+    } catch (error) {setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return ( <div className="bg-slate-950 w-full h-screen">
     
     <div className='flex p-40 max-w-53xl mx-auto flex-col md:flex-row md:items-center gap-40'>
@@ -26,20 +60,29 @@ export default function SignUp() {
         
          {/*right */}
         <div className='flex-1 '>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value='Your UserName' className='text-slate-200'/>
-              <TextInput type='text' placeholder='Username' id='username' />
+              <TextInput type='text' placeholder='Username' id='username' onChange={handleChange} />
             </div>
             <div>
               <Label value='Your E-mail'className='text-slate-200' />
-              <TextInput type='text' placeholder='E-mail' id='email' />
+              <TextInput type='email' placeholder='E-mail' id='email' onChange={handleChange} />
             </div>
             <div>
               <Label value='Your Password' className='text-slate-200' />
-              <TextInput type='text' placeholder='Password' id='password' />
+              <TextInput type='password' placeholder='Password' id='password' onChange={handleChange} />
             </div>
-            <Button className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white' type='submit'>Sign Up</Button>
+            <Button className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white' type='submit' disabled={loading}>
+            {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )} 
+              </Button>
           </form>
 
           <div className='flex gap-3 text-sm mt-5'>
@@ -47,10 +90,15 @@ export default function SignUp() {
             <Link to='/SignIn' className='text-blue-500'>Sign In 
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
         </div >
 
       </div>
     </div>  
     
-  )
+  );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/Api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
